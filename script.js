ed5db2bcb60f9e3730d5ecfd0510b7bf1e8b15c7
Zerton <zrt0nnkra6@gmail.com>
2026-02-13 23:57:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const wrapper = document.getElementById("wrapper");
+const question = document.getElementById("question");
+const gif = document.getElementById("gif");
+const yesBtn = document.getElementById("yes-btn");
+const noBtn = document.getElementById("no-btn");
+
+function go() {
+  window.location.href = "happy.html";
+};
+noBtn.addEventListener("click", () => {
+  window.location.href = "sad.html";
+  question.innerHTML = "I feel do bad about this:(";
+  gif.src = "./gifs/sad.gif";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
